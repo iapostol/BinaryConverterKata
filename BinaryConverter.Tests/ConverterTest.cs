8e505239af4c1f83eddaa410deedae5c92affc97
Ion Apostol <apostolin@gmail.com>
2024-02-17 14:28:04 +0200

    Convert to binary to decimal

--- BinaryConverter.Tests/ConverterTest.cs
+++ BinaryConverter.Tests/ConverterTest.cs
@@ -1,5 +1,8 @@
 using Xunit;
 using FluentAssertions;
+using System.Collections.Generic;
+using System;
+using System.IO;
 
 namespace BinaryConverter.Tests
 {
@@ -14,9 +17,23 @@ namespace BinaryConverter.Tests
         [InlineData(5, "101")]
         [InlineData(174, "10101110")]
         [InlineData(1981, "11110111101")]
-        public void FromDecimalToBinary(int number, string binary)
+        public void DecimalToBinary(int number, string binary)
         {
             Converter.BinaryFor(number).Should().Be(binary);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("1", 1)]
+        [InlineData("10", 2)]
+        [InlineData("11", 3)]
+        [InlineData("100", 4)]
+        [InlineData("101", 5)]
+        [InlineData("10101110", 174)]
+        [InlineData("11110111101", 1981)]
+        public void BinaryToDecimal(string binary, int number)
+        {
+            Converter.DecimalFor(binary).Should().Be(number);
         }
     }
 }
--- BinaryConverter/Converter.cs
+++ BinaryConverter/Converter.cs
@@ -15,6 +15,24 @@ public static class Converter
         return Reverse(binary);
     }
 
+    public static int DecimalFor(string binary)
+    {
+        if (string.IsNullOrEmpty(binary))
+            throw new ArgumentNullException("binary is empty");
+
+        if (binary.Length == 1)
+            return int.Parse(binary);
+
+        var number = 0;
+
+        var length = binary.Length;
+
+        for (int i = 0, power = length - 1; i < length; i++, power--)
+            number += int.Parse(binary[i].ToString()) * (int)Math.Pow(2, power);
+
+        return number;
+    }
+
     private static string Reverse(string str)
     {
         var charArray = str.ToCharArray();
